6957e89a2faddcefbf2025bb410168df472541ca
Christian Ayscue <coayscue@gmail.com>
2026-03-24 11:56:34 -0700

    update prod api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5001/api'
-});
+const baseURL = import.meta.env.PROD
+  ? 'https://api.saunaman-sf.com/api'
+  : 'http://localhost:5001/api';
+
+const api = axios.create({ baseURL });
 
 export default api;
