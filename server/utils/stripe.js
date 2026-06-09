109528ebdcf68fadf31c226128b9af9ae56f3bb1
Christian Ayscue <coayscue@gmail.com>
2026-06-09 10:27:15 -0700

    save

--- server/utils/stripe.js
+++ server/utils/stripe.js
@@ -1,13 +1,10 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const PAYMENT_METHOD_TYPES =
-  process.env.NODE_ENV === "production" ? ["apple_pay", "card"] : ["card"];
-
 async function createPaymentIntent(amountInDollars) {
   const paymentIntent = await stripe.paymentIntents.create({
     amount: Math.round(amountInDollars * 100),
     currency: "usd",
-    payment_method_types: PAYMENT_METHOD_TYPES,
+    payment_method_types: ["card"],
   });
   return paymentIntent;
 }
